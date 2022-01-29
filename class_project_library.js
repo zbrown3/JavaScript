ec80d90acb9799101b71036bb4195daaa48e39f2
zbrown3 <ziare.brown@gmail.com>
2022-01-29 17:02:31 -0600

    general update

--- class_project_library.js
+++ class_project_library.js
@@ -17,6 +17,66 @@ class Media {
     set isCheckedOut(CheckedOut) {
         this._isCheckedOut = CheckedOut;
     }
+    toggleCheckOutStatus() {
+        if (this._isCheckedOut === true) {
+            this._isCheckedOut = false;
+        } else {
+            this._isCheckedOut = true;
+        }
+    }
+    getAverageRating() {
+        let ratings = this._ratings;
+        let sumRatings = ratings.reduce((accumulator,currentValue) => {
+            return accumulator + currentValue;
+        })
+        return sumRatings / ratings.length;
+
+    }
+    addRatings(newRating) {
+        this._ratings.push(newRating);
+    }
 }
-const profootballtalk = new Media('PFTLive');
-console.log(profootballtalk.isCheckedOut);
+
+class Book extends Media {
+    constructor(author,title,pages) {
+        super(title);
+        this._author = author;
+        this._pages = pages;
+    }
+    get author() {
+        return this._author;
+    }
+    get pages() {
+        return  this._pages;
+    }
+}
+
+class Movie extends Media {
+    constructor(director,title,runTime) {
+        super(title);
+        this._director = director;
+        this._runTime = runTime;
+    }
+    get director() {
+        return this._director;
+    }
+    get runTime() {
+        return  this._runTime;
+    }
+}
+
+const historyOfEverything = new Book('Bill Bryson','A Short History of Nearly Everything',544);
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRatings(4);
+historyOfEverything.addRatings(5);
+historyOfEverything.addRatings(5);
+console.log(historyOfEverything.getAverageRating());
+
+const speed = new Movie('Jan de Bont','Speed',116);
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+speed.addRatings(1);
+speed.addRatings(1);
+speed.addRatings(5);
+console.log(speed.getAverageRating());
